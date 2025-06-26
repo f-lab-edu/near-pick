f0efc9513c60c5c626b733b13a10bdfd8e0934e8
marin13 <marin@exntu.com>
2025-06-26 21:25:28 +0900

    chore: 프로젝트 초기 세팅 및 공통 설정 추가

---
+++ src/test/kotlin/com/nearpick/NearpickBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nearpick
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NearpickBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
