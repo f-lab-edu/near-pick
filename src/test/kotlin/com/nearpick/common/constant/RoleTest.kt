0e8f0a6547054d4c6e5e1457679a462047b1588a
marin13 <marin@exntu.com>
2025-08-21 16:01:29 +0900

    refactor: role 테스트코드에 중복 로직 제거

--- src/test/kotlin/com/nearpick/common/constant/RoleTest.kt
+++ src/test/kotlin/com/nearpick/common/constant/RoleTest.kt
@@ -16,29 +16,18 @@ class RoleTest : StringSpec({
     //  ADMIN은 회원가입을 통한 생성이 불가능하고 직접 데이터를 넣는 방법만 가능
     // TODO: 추후 추가 개발 시 ADMIN 권한 계정을 생성하는 정책을 따로 설정할 예정
     "ADMIN 권한은 회원가입을 통해 생성이 불가능하므로 InvalidRoleException 발생한다" {
-        shouldThrowExactly<InvalidRoleException> {
-            Role.from("ADMIN")
-        }
-        shouldThrowExactly<InvalidRoleException> {
-            Role.from("admin")
+        listOf("ADMIN", "admin").forEach { item ->
+            shouldThrowExactly<InvalidRoleException> {
+                Role.from(item)
+            }
         }
     }
 
     "null이나 빈 문자열, 존재하지 않는 권한의 문자열이 들어온 경우 권한을 확인할 수 없으므로 InvalidRoleException을 발생한다." {
-        shouldThrowExactly<InvalidRoleException> {
-            Role.from("MANAGER")
-        }
-
-        shouldThrowExactly<InvalidRoleException> {
-            Role.from("GUEST")
-        }
-
-        shouldThrowExactly<InvalidRoleException> {
-            Role.from(null)
-        }
-
-        shouldThrowExactly<InvalidRoleException> {
-            Role.from("")
+        listOf("MANAGER", "GUEST", null, "").forEach { item ->
+            shouldThrowExactly<InvalidRoleException> {
+                Role.from(item)
+            }
         }
     }
 })
